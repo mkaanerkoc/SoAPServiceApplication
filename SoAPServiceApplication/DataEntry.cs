e3679906827c6967168052756474a2d154f3af93
Mehmet Kaan ERKOÇ <mkaanerkoc@gmail.com>
2017-05-24 15:33:35 +0300

    Created
    SoAP service and LINQtoSQL interface implemented

---
+++ SoAPServiceApplication/DataEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoAPServiceApplication
+{
+    public class DataEntry
+    {
+        public int channel { get; set; }
+        public DateTime dateTime { get; set; }
+        public String units { get; set; }
+        public String channelName { get; set; }
+        public double value { get; set; }
+        public int status { get; set; }
+        //private int saveCol;
+    }
+}
---
+++ SoAPServiceApplication/SoAPService.asmx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SoAPServiceApplication
+{
+    /// <summary>
+    /// Summary description for SoAPService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SoAPService : System.Web.Services.WebService
+    {
+
+        const string  connectString = "Data Source=LAPTOP-H740Q104;Initial Catalog=EnvidasUzunDere;Integrated Security=True";
+
+        DataClasses1DataContext db = new DataClasses1DataContext(connectString);
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public List<DataEntry> Response()
+        {
+            List<DataEntry> dataList = new List<DataEntry>();
+            var channelCount = db.Channels.Select(s => s).ToArray().Length;
+            var datas = db.InstantDatas.OrderByDescending(s => s.Date_Time).Take(channelCount).ToArray();
+            Array.Sort(datas, delegate (InstantData x, InstantData y) { return x.channel.CompareTo(y.channel); });
+            for (int i = 0; i < datas.Length; i++)
+            {
+                DataEntry entry = new DataEntry();
+                entry.channelName = datas[i].Channels.name;
+                entry.value = datas[i].value;
+                entry.channel = datas[i].channel;
+                entry.dateTime = datas[i].Date_Time;
+                entry.status = datas[i].status;
+                entry.units = datas[i].Channels.units;
+                dataList.Add(entry);
+            }
+            return dataList;
+        }
+    }
+}
